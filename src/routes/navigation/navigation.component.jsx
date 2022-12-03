1a5393cbd4b9c08550581c37fef167a828791ecf
Victor Leite <victor_leite@outlook.com.br>
2022-12-03 12:02:11 -0300

    feat: add router with navigation, home and shops components

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Link, Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import './navigation.styles.scss';
+import {ReactComponent as MarketplaceLogo} from '../../assets/crown.svg'
+
+const Navegation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+          <div><MarketplaceLogo className='logo'/></div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shops">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navegation;
